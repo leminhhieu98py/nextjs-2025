93c0659afa2f8602f56d97f87b41717b9ae3b06b
leminhhieu98py <leminhhieu98py@gmail.com>
2025-01-08 11:30:35 +0700

    feat: add global not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Can not found the page with current URL. Please try again!</p>
+    </main>
+  );
+}
