4f17b97698f5c986843bd0ccc1fb809f1d227873
leminhhieu98py <leminhhieu98py@gmail.com>
2025-01-21 22:58:45 +0700

    feat: add static page metadata

--- 07-optimizations/app/feed/page.js
+++ 07-optimizations/app/feed/page.js
@@ -1,6 +1,11 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export const metadata = {
+  title: 'All Posts',
+  description: 'Browse our all posts'
+};
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
