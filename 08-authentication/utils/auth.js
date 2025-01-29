9131cf7d98816b9a71a0d12cd2d68e14cf0ea969
leminhhieu98py <leminhhieu98py@gmail.com>
2025-01-29 22:48:31 +0700

    chore: brainstorm todo steps

---
+++ 08-authentication/utils/auth.js
@@ -0,0 +1,6 @@
+// TODO: Prerequisite: create a valid lucia instance
+
+// TODO: 1. generateSessionToken
+// TODO: 2. createSession
+// TODO: 3. validateSessionToken
+// TODO: 4. invalidateSessionToken
