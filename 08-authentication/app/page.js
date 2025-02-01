65c74095b1de9c66c9054f044dd14f6260086610
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-01 21:45:03 +0700

    feat: prevent access login page after login and protect route training

--- 08-authentication/app/page.js
+++ 08-authentication/app/page.js
@@ -1,6 +1,13 @@
 import AuthForm from '@/components/auth-form';
+import { validateAuth } from '@/utils/auth';
+import { redirect } from 'next/navigation';
 
 // TODO: add login mode from search params
 export default async function Home() {
+  const { user } = await validateAuth();
+  if (user) {
+    redirect('/training');
+  }
+
   return <AuthForm />;
 }
--- 08-authentication/app/training/page.js
+++ 08-authentication/app/training/page.js
@@ -1,6 +1,13 @@
 import { getTrainings } from '@/service/training/service';
+import { validateAuth } from '@/utils/auth';
+import { redirect } from 'next/navigation';
 
 export default async function TrainingPage() {
+  const { user } = await validateAuth();
+  if (!user) {
+    redirect('/');
+  }
+
   const trainingSessions = getTrainings();
 
   return (
