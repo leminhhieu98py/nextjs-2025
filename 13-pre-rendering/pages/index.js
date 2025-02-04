b787d9737f34b4091579950080c11e1aff0886a2
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-04 21:48:35 +0700

    feat: add data from JSON file for getStaticProps function

--- 13-pre-rendering/pages/index.js
+++ 13-pre-rendering/pages/index.js
@@ -1,17 +1,13 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 export async function getStaticProps() {
+  const pathName = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(pathName);
+  const data = JSON.parse(jsonData);
+
   return {
-    props: {
-      products: [
-        {
-          id: 1,
-          title: 'P1'
-        },
-        {
-          id: 2,
-          title: 'P2'
-        }
-      ]
-    }
+    props: data
   };
 }
 
