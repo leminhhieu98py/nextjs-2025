c4e61cb04f691e2dcdfb70271914d809486e68b8
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-06 22:06:56 +0700

    feat: add form and handle API routes

--- 16-api-routes-starting-project/pages/api/feedback/index.js
+++ 16-api-routes-starting-project/pages/api/feedback/index.js
@@ -1,5 +1,27 @@
-function handler(req, res) {
-  res.status(200).json({ message: 'Hello from Otis' });
+import fs from 'fs/promises';
+import path from 'path';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, feedback } = req.body;
+
+    const feedbackData = {
+      id: new Date().toISOString(),
+      email,
+      feedback
+    };
+
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const file = await fs.readFile(filePath);
+    const fileData = JSON.parse(file);
+    fileData.push(feedbackData);
+
+    fs.writeFile(filePath, JSON.stringify(fileData));
+
+    res.status(201).json({ message: 'Add feedback' });
+  } else {
+    console.log('No no!!!');
+  }
 }
 
 export default handler;
--- 16-api-routes-starting-project/pages/index.js
+++ 16-api-routes-starting-project/pages/index.js
@@ -1,7 +1,45 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailRef = useRef(null);
+  const feedbackRef = useRef(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const feedback = feedbackRef.current.value;
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        feedback
+      }),
+      headers: {
+        'Content-Type': 'Application/json'
+      }
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form id="auth-form" onSubmit={handleSubmit}>
+        <p>
+          <label htmlFor="email">Email</label>
+          <input ref={emailRef} type="email" name="email" id="email" />
+        </p>
+        <p>
+          <label htmlFor="feedback">Feedback</label>
+          <textarea ref={feedbackRef} name="feedback" id="feedback" />
+        </p>
+        <p>
+          <button type="submit">Send feedback</button>
+        </p>
+      </form>
     </div>
   );
 }
