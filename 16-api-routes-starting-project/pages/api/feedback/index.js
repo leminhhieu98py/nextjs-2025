911fcc6a0fa6abdf67eb32ffc2c635f1fd0122ea
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-06 22:22:35 +0700

    feat: add GET API routes and handle refresh list after adding a new feedback

--- 16-api-routes-starting-project/pages/api/feedback/index.js
+++ 16-api-routes-starting-project/pages/api/feedback/index.js
@@ -1,6 +1,15 @@
 import fs from 'fs/promises';
 import path from 'path';
 
+const getFilePath = () => path.join(process.cwd(), 'data', 'feedback.json');
+
+const getData = async (filePath) => {
+  const file = await fs.readFile(filePath);
+  const fileData = JSON.parse(file);
+
+  return fileData;
+};
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, feedback } = req.body;
@@ -11,16 +20,19 @@ async function handler(req, res) {
       feedback
     };
 
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-    const file = await fs.readFile(filePath);
-    const fileData = JSON.parse(file);
+    const filePath = getFilePath();
+    const fileData = await getData(filePath);
+
     fileData.push(feedbackData);
 
     fs.writeFile(filePath, JSON.stringify(fileData));
 
     res.status(201).json({ message: 'Add feedback' });
   } else {
-    console.log('No no!!!');
+    const filePath = getFilePath();
+    const fileData = await getData(filePath);
+
+    res.status(200).json({ data: fileData });
   }
 }
 
--- 16-api-routes-starting-project/pages/index.js
+++ 16-api-routes-starting-project/pages/index.js
@@ -1,8 +1,15 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function HomePage() {
   const emailRef = useRef(null);
   const feedbackRef = useRef(null);
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  const getFeedbacks = async () => {
+    fetch('/api/feedback')
+      .then((res) => res.json())
+      .then((data) => setFeedbacks(data.data));
+  };
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -21,26 +28,43 @@ function HomePage() {
       }
     })
       .then((res) => res.json())
-      .then((res) => console.log(res));
+      .then(getFeedbacks);
   }
 
+  useEffect(() => {
+    getFeedbacks();
+  }, []);
+
   return (
-    <div>
-      <h1>The Home Page</h1>
-      <form id="auth-form" onSubmit={handleSubmit}>
-        <p>
-          <label htmlFor="email">Email</label>
-          <input ref={emailRef} type="email" name="email" id="email" />
-        </p>
-        <p>
-          <label htmlFor="feedback">Feedback</label>
-          <textarea ref={feedbackRef} name="feedback" id="feedback" />
-        </p>
-        <p>
-          <button type="submit">Send feedback</button>
-        </p>
-      </form>
-    </div>
+    <>
+      <div>
+        <h1>The Home Page</h1>
+        <form id="auth-form" onSubmit={handleSubmit}>
+          <p>
+            <label htmlFor="email">Email</label>
+            <input ref={emailRef} type="email" name="email" id="email" />
+          </p>
+          <p>
+            <label htmlFor="feedback">Feedback</label>
+            <textarea ref={feedbackRef} name="feedback" id="feedback" />
+          </p>
+          <p>
+            <button type="submit">Send feedback</button>
+          </p>
+        </form>
+      </div>
+      <hr />
+      <div>
+        <ul>
+          {feedbacks.map((feedback) => (
+            <li key={feedback.id}>
+              <p>From: {feedback.email}</p>
+              <p>{feedback.feedback}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
